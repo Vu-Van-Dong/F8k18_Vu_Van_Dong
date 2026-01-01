de1c8a9e77c444b9f41154fd957b8be468cd207d
dong <vuvandong10022004@gmail.com>
2026-01-01 09:33:06 +0700

    Buoi 21 -Css flex

---
+++ js/30-12-2025/main.js
@@ -0,0 +1,76 @@
+// const  numbers = [1 ,2,3,4,5,1000]
+// console.log(numbers)
+// console.log(numbers.length)
+//
+// console.log(numbers[numbers.length-1 ] )
+
+// numbers[0]=40
+// console.log(numbers)
+//
+// const  copieNumbers = numbers
+//
+// copieNumbers[1] = 2020
+//  console.log(numbers)
+//
+// const a = 1
+// let b = a
+//
+// b =2
+// console.log(a)
+//
+//
+
+
+// const studentA = {
+// 		name: "A",
+// 		age: 25,
+// 		scope: [0, 1, 2]
+// }
+
+// console.log(studentA.name,studentA.age)
+// console.log(studentA.scope[1])
+//
+//
+// const scores = studentA.scope
+// scores[0] = 10
+// console.log(studentA.scope)
+//
+//
+// const studentB = studentA
+// studentB.name = 'B'
+//
+// console.log(studentB)
+
+
+
+
+
+
+
+
+
+// let a = "1dfghjk"
+// let b = "1"
+// let c = a /b
+//
+// console.log(c)
+
+
+
+
+
+
+
+
+
+const a = true
+const b = false
+const c = a || b
+console.log(c)
+
+
+
+
+
+
+
