ff9c35e75c181f87a8049e67f0bd1afa8e64158f
dong <vuvandong10022004@gmail.com>
2026-03-13 23:44:28 +0700

    Buoi 35

---
+++ js/10-03-2026(Buoi 35)/main.js
@@ -0,0 +1,13 @@
+
+
+
+
+import  z from "https://cdn.jsdelivr.net/npm/zod@4.3.6/+esm"
+
+
+
+
+
+
+
+
---
+++ js/ontap/mian.js
@@ -0,0 +1,20 @@
+
+
+
+const  colors =['mau' , 'tam' , 'ba']
+
+for (const color of colors) {
+		console.log(color)
+}
+
+
+
+
+
+
+
+
+
+
+
+
